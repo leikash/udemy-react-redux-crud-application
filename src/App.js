381e419be2b9ca58c0600fed1c73148e8658b42a
leikash <yukio.yamada.yy@gmail.com>
2021-08-19 12:57:17 +0800

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,42 @@ import React from "react";
 // //  return <h1>Hello, world!</h1>;
 //   return dom;
 // }
+// function App() {
+// //   return <input type="text" onClick={() => {console.log("It's clicked")}}></input>;
+// //   return <input type="text" onChange={() => {console.log("It's changed!")}}></input>;
+//   return (
+//     // <div>
+//     //   <label htmlFor="bar">bar</label>
+//     //   <input type="text" onChange={() => {console.log("It's changed!")}} />;
+//     // </div>
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("It's changed!")}} />;
+//     </React.Fragment>
+//   )
+//  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("It's changed!")}} />;
+//     </React.Fragment>
+//     )
+//   }
+// }
 
-function App() {
-//   return <input type="text" onClick={() => {console.log("It's clicked")}}></input>;
-//   return <input type="text" onChange={() => {console.log("It's changed!")}}></input>;
-  return (
-    // <div>
-    //   <label htmlFor="bar">bar</label>
-    //   <input type="text" onChange={() => {console.log("It's changed!")}} />;
-    // </div>
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("It's changed!")}} />;
-    </React.Fragment>
-  )
- }
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+}
 
+const Cat  = () => {
+  return <div>Meow!</div>;
+}
 
 export default App;
