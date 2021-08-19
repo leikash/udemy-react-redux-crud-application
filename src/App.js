ba6a650cd81c5f0c24e0bb3a28766816daf3c0f6
leikash <yukio.yamada.yy@gmail.com>
2021-08-19 12:34:02 +0800

    practice jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
+// function App() {
+//   const greeting = "Hi, Tom!";
+//   const dom = <h1 className="foo">{greeting}</h1>;
+// //  return <h1>Hello, world!</h1>;
+//   return dom;
+// }
+
 function App() {
-  return <h1>Hello, world!</h1>
-}
+//   return <input type="text" onClick={() => {console.log("It's clicked")}}></input>;
+//   return <input type="text" onChange={() => {console.log("It's changed!")}}></input>;
+  return (
+    // <div>
+    //   <label htmlFor="bar">bar</label>
+    //   <input type="text" onChange={() => {console.log("It's changed!")}} />;
+    // </div>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("It's changed!")}} />;
+    </React.Fragment>
+  )
+ }
+
 
 export default App;
